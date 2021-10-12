708e17f886bdc29870ffb99c866bf341d320b467
Tuhin <bhowmicktuhin34@gmail.com>
2021-10-12 21:44:24 +0530

    changed the title

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Converter',
         debugShowCheckedModeBanner: false,
         darkTheme: ThemeData(
           colorScheme: ColorScheme.highContrastDark(),
